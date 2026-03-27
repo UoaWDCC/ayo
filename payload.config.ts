346b2ee26cc1c6be3924bf9c4762638b0f8317bb
Sam <208591791+sswez02@users.noreply.github.com>
2026-03-28 00:39:32 +1300

    feat: set up mongodb atlas connection for payload (#6)

---
+++ payload.config.ts
@@ -0,0 +1,15 @@
+import { buildConfig } from 'payload'
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
+
+export default buildConfig({
+  secret: process.env.PAYLOAD_SECRET || '',
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URL || '',
+    // connectOptions: {
+    //   dbName: 'wdcc-ayo',
+    // },
+  }),
+  collections: [
+    // add collections
+  ],
+})
