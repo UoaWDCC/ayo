eeee37cbbf31960633335fca2ffd4d479af33337
simon <simon.z.2237@gmail.com>
2026-04-22 22:09:49 +1200

    feat: add event components

---
+++ src/app/(frontend)/components/EventsBlock.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EventsCard from './EventsCard'
+const Events = () => {
+  return (
+    <div className="text-black w-full">
+      <h1>Concert & Events</h1>
+      <EventsCard />
+    </div>
+  )
+}
+
+export default Events
---
+++ src/app/(frontend)/components/EventsCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Image from 'next/image'
+const EventsCard = () => {
+  return (
+    <div className="border-y border-gray-400">
+      <Image src="/grey_rectangle.png" width={250} height={250} alt="Grey Rectangle" />
+    </div>
+  )
+}
+
+export default EventsCard
