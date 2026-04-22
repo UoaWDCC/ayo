0b2a98da99cb9ccb0a0d5cf8c60df127a260d51a
simon <simon.z.2237@gmail.com>
2026-04-23 00:24:35 +1200

    feat: added theme font family and font sizes

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -16,43 +16,25 @@ export default async function HomePage() {
   const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
 
   return (
-    <div className="home">
-      <div className="content">
-        <picture>
-          <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
-          <Image
-            alt="Payload Logo"
-            height={65}
-            src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
-            width={65}
-          />
-        </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
-        <div className="links">
-          <a
-            className="admin"
-            href={payloadConfig.routes.admin}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Go to admin panel
-          </a>
-          <a
-            className="docs"
-            href="https://payloadcms.com/docs"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Documentation
-          </a>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="home font-sans text-center w-4xl">
+        <h1 className="text-heading">Placeholder Text</h1>
+
+        <div className="text-body">
+          <p>
+            Auckland Youth Orchestra is the premier regional youth orchestra in New Zealand. Founded
+            in 1948, and originally named the Auckland Junior Symphony Orchestra (or AJSO), it was
+            the first youth orchestra established in the Southern Hemisphere. It is designed to
+            bridge the gap between school orchestras and adult professional groups. The role of AYO
+            is not just to perform concerts but to train future musicians to feed into our national
+            ensembles and orchestras. We also play an important audience development role as many of
+            our young audience members are likely to be the future supporters of the arts. AYO is a
+            vibrant social experience for young people and makes an important contribution to the
+            cultural life of Auckland and New Zealand. No other orchestral organisation or
+            institution in New Zealand provides such a comprehensive programme to musicians up to
+            the age of 26.
+          </p>
         </div>
-      </div>
-      <div className="footer">
-        <p>Update this page by editing</p>
-        <a className="codeLink" href={fileURL}>
-          <code>app/(frontend)/page.tsx</code>
-        </a>
       </div>
     </div>
   )
