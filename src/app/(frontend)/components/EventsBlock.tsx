d44b8f3d713b0453f0fda49793f8693fad4f9fde
simon <simon.z.2237@gmail.com>
2026-04-24 01:13:13 +1200

    feat: added year and month dropdown

--- src/app/(frontend)/components/EventsBlock.tsx
+++ src/app/(frontend)/components/EventsBlock.tsx
@@ -3,11 +3,28 @@ import EventsCard from './EventsCard'
 const Events = () => {
   return (
     <div className="text-black w-full">
-      <div className="text-heading font-semibold my-10 ml-35">
-        <h1>Concerts & Events</h1>{' '}
+      <div className="text-body  my-10 ml-35">
+        <h1 className="text-heading font-semibold">Concerts & Events</h1>
+        <div className="flex mt-5">
+          <p className="mr-6 text-[#B2B2B2]"> Year </p>
+          <select className="font-bold appearance-none">
+            <option value="2026">2026</option>
+            <option value="2027">2027</option>
+          </select>
+
+          <p className="ml-15 mr-6 text-[#B2B2B2]"> Month </p>
+          <select className="font-bold appearance-none">
+            <option value="June">June</option>
+            <option value="July">July</option>
+          </select>
+        </div>
       </div>
+
       <div className="flex justify-center">
         <EventsCard />
+      </div>
+      <div className="">
+        <p></p>
       </div>
     </div>
   )
