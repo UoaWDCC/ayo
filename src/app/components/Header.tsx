b9025609262821db2a06bd7b969e43d0d154a122
MaanasNegi <negimanas11@gmail.com>
2026-04-25 21:51:51 +1200

    feat: add basic header layout with links and logo

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,35 @@
+import Link from 'next/link'
+
 export default function Header() {
   return (
-    <header>
-      <p>Header</p>
+    <header className="bg-black text-white">
+      {/* Container to separate content */}
+      <div className="flex items-center px-6 py-8">
+        {/* Left container */}
+        <div>
+          {/* AYO logo png as placeholder, replace with new logo svg when available */}
+          <img src="/ayo-logo-white.png" alt="AYO Logo" className="w-50 h-auto ml-20" />
+        </div>
+
+        {/* Right Navbar */}
+        <nav className="flex flex-1 ml-100 mr-40 justify-between text-2xl font-semibold">
+          <Link href="/" className="text-white hover:opacity-70">
+            HOME
+          </Link>
+          <Link href="/about" className="text-white hover:opacity-70">
+            ABOUT US
+          </Link>
+          <Link href="/concert" className="text-white hover:opacity-70">
+            CONCERT & EVENTS
+          </Link>
+          <Link href="/join" className="text-white hover:opacity-70">
+            JOIN AYO
+          </Link>
+          <Link href="/support" className="text-white hover:opacity-70">
+            SUPPORT US
+          </Link>
+        </nav>
+      </div>
     </header>
   )
 }
