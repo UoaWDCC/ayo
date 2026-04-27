6a0c8c9bc77e26ccbe9c206f876ca3cf5253733d
MaanasNegi <negimanas11@gmail.com>
2026-04-27 14:34:37 +1200

    fix: change gap sizes for better responsiveness

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,7 +6,8 @@ export default function Header() {
       <div className="flex flex-col lg:flex-row items-center justify-between px-8 lg:px-24 py-8 xl:px-28 gap-4 lg:gap-0">
         <img src="/ayo-logo-white.png" alt="AYO Logo" className="w-36 lg:w-52 h-auto shrink-0" />
 
-        <nav className="flex flex-wrap justify-center lg:justify-end gap-4 lg:gap-10 xl:gap-14 lg:ml-16 items-center text-sm lg:text-xl xl:text-2xl font-semibold">
+        {/* <nav className="flex flex-wrap justify-center lg:justify-end gap-4 lg:gap-10 xl:gap-14 lg:ml-16 items-center text-sm lg:text-xl xl:text-2xl font-semibold"> */}
+        <nav className="hidden lg:flex flex-wrap justify-center lg:justify-end gap-4 lg:gap-6 xl:gap-10 lg:ml-8 xl:ml-16 items-center text-base lg:text-lg xl:text-2xl font-semibold">
           <Link href="/" className="hover:opacity-70">
             HOME
           </Link>
