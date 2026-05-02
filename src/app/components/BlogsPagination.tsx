03f64cb132897cd85a585132f69a2e8f2b600541
Ron Arzadon <sirronaldarzadon@gmail.com>
2026-05-02 21:43:33 +1200

    feat: simple pagination

--- src/app/components/BlogsPagination.tsx
+++ src/app/components/BlogsPagination.tsx
@@ -1,3 +1,21 @@
+'use client'
+import { useState } from 'react'
+
 export default function BlogsPagination() {
-  return <div>PAGINATION YES</div>
+  const [currentPage, setCurrentPage] = useState(1)
+  const totalPages = 3
+
+  return (
+    <div>
+      <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1}>
+        Previous
+      </button>
+      <span>
+        {currentPage} of {totalPages}
+      </span>
+      <button onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === totalPages}>
+        Next
+      </button>
+    </div>
+  )
 }
