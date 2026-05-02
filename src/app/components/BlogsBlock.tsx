685916dc76626becf00e8130cc455ca094eb80a8
Ron Arzadon <sirronaldarzadon@gmail.com>
2026-05-02 16:00:55 +1200

    feat: hardcoded blog card

--- src/app/components/BlogsBlock.tsx
+++ src/app/components/BlogsBlock.tsx
@@ -7,7 +7,21 @@ export default function BlogsBlock() {
     <div>
       <h1>News</h1>
       <BlogsFilter />
-      <BlogsCard />
+      <BlogsCard
+        title="2025 Soloist Competition"
+        date="November 1st, 2025"
+        description="The Final of the 2025 AYO Soloist Competition was held on Sunday, 19 October 2025.  It was an exciting event with the wonderful talent of AYO’s players being showcased once more."
+      />
+      <BlogsCard
+        title="Hear Tony Yan Tong Chen being interviewed about our June concert series on RNZ Concert"
+        date="June 6th, 2025"
+        description="AYO’s soloist for the June concert series Tony Yan Tong Chen was interviewed by Bryan Crump on RNZ Concert on 4 June 2025 – hear the interview and some of his recordings here!"
+      />
+      <BlogsCard
+        title="Update – Howick June Concert CANCELLED"
+        date="June 6th, 2025"
+        description="With less than two weeks to go until the first concert of this series we regret inform you that our popular Howick venue, the All Saints Church, has suffered significant flooding and is..."
+      />
       <BlogsPagination />
     </div>
   )
--- src/app/components/BlogsCard.tsx
+++ src/app/components/BlogsCard.tsx
@@ -1,3 +1,15 @@
-export default function BlogsFilter() {
-  return <div>FILTER YES</div>
+type Blog = {
+  title: string
+  date: string
+  description: string
+}
+
+export default function BlogsCard(Blog: Blog) {
+  return (
+    <div>
+      <h2>{Blog.title}</h2>
+      <p>{Blog.date}</p>
+      <p>{Blog.description}</p>
+    </div>
+  )
 }
