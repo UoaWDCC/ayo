f05ee1b5347a0b25e5880230735e137833623da5
sswez02 <208591791+sswez02@users.noreply.github.com>
2026-05-04 19:59:02 +1200

    Fix missing landing page component

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import AboutIntro from './components/AboutIntro'
 import EventsBlock from './components/events/EventsBlock'
 import BlogsBlock from './components/blogs/BlogsBlock'
 
 export default function LandingPage() {
   return (
     <main className="min-h-screen bg-white text-black">
+      <AboutIntro />
       <EventsBlock />
       <BlogsBlock />
     </main>
