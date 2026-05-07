8296aca9018d09ad8effaa2d57572b4f1e85cc21
twkavya004 <twkavya004@gmail.com>
2026-05-07 13:15:41 +1200

    feat: added pages and basic layout for faqs section

--- src/app/(frontend)/components/FAQSection.tsx
+++ src/app/(frontend)/components/FAQSection.tsx
@@ -1,6 +1,5 @@
 import FAQItem from './FAQItem'
 
-// Sample FAQ data to be replaced with actual FAQs
 const faqData = [
   {
     id: 1,
@@ -52,11 +51,13 @@ const FAQSection = () => {
       <h2 className="font-semibold text-[40px] leading-[56px] text-black mx-8 md:mx-20 lg:mx-25 xl:mx-30 pt-[116px] pb-[34px]">
         FAQs
       </h2>
+
       <div>
         {faqData.map((item) => (
           <FAQItem key={item.id} {...item} />
         ))}
       </div>
+
       <div className="h-[64px]" />
     </section>
   )
--- src/app/(frontend)/join-ayo/page.tsx
+++ src/app/(frontend)/join-ayo/page.tsx
@@ -3,7 +3,6 @@ import FAQSection from '../components/FAQSection'
 export default function JoinAyoPage() {
   return (
     <main>
-      {/* <p>This is the join AYO page</p> */}
       <FAQSection />
     </main>
   )
