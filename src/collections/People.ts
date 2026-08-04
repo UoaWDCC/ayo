2cab8352f28cc30579c3e2c695d69e95d082844f
limmcary <limmcary@gmail.com>
2026-08-04 22:16:55 +1200

    chore: created People.ts in the collections directory

---
+++ src/collections/People.ts
@@ -0,0 +1,12 @@
+import type { CollectionConfig } from 'payload'
+
+export const People: CollectionConfig = {
+    slug: 'people',
+    fields: [
+        {
+            name: 'name',
+            type: 'text',
+            required: true
+        }
+        ]
+}
