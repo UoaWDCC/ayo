97b73c467e772b040c91c2eacf23f9a33b5c5c06
limmcary <limmcary@gmail.com>
2026-08-12 12:50:20 +1200

    feat: add role fields - role and sortOrder

--- src/collections/Roles.ts
+++ src/collections/Roles.ts
@@ -2,5 +2,16 @@ import type { CollectionConfig } from 'payload'
 
 export const Roles: CollectionConfig = {
     slug: 'roles',
-    fields: []
+    fields: [
+        {
+            name: 'role',
+            type: 'text',
+            required: true,
+        },
+        {
+            name: 'sortOrder',
+            type: 'number',
+            required: true,
+        }
+    ]
 }
