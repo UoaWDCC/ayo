bda1a1b00b32e741871307ffd353acb4cce8b9d9
Kavya Thangella <twkavya004@gmail.com>
2026-08-15 11:26:26 +1200

    feat: added FAQBlock in payload

---
+++ src/blocks/FAQBlock.ts
@@ -0,0 +1,33 @@
+import type { Block } from 'payload'
+
+/**
+ * FAQ block:
+ *
+ * Used to display a frequently asked question with an answer.
+ * - question: The question text
+ * - answer: The answer text
+
+*/
+
+export const FAQBlock: Block = {
+  slug: 'faq',
+  fields: [
+    {
+      name: 'rows',
+      type: 'array',
+      required: true,
+      fields: [
+        {
+          name: 'question',
+          type: 'text',
+          required: true,
+        },
+        {
+          name: 'answer',
+          type: 'richText',
+          required: true,
+        },
+      ],
+    },
+  ],
+}
