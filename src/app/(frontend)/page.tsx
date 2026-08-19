af6430e1d01be087f01a9a4fed30083032666438
Kavya Thangella <twkavya004@gmail.com>
2026-08-20 11:13:11 +1200

    feat/fix: commented out AboutIntro and added AboutUsQuoteVid to home page

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero'
 import AboutIntro from './components/AboutIntro'
 import EventsBlock from './components/events/EventsBlock'
 import BlogsBlock from './components/blogs/BlogsBlock'
+import AboutUsQuoteVid from './components/AboutUsQuoteVid'
 
 import { getPageBySlug } from '@/lib/getPageBySlug'
 import type { Media } from '@/payload-types'
@@ -25,7 +26,17 @@ export default async function LandingPage() {
         title="Here Plays The Future"
         backgroundImage={heroImageUrl ?? '/hero-placeholder.jpg'}
       />
-      <AboutIntro />
+      {/* <AboutIntro /> */}
+
+      <div className="w-full mt-10">
+        <AboutUsQuoteVid
+          quote="Watching Auckland Youth Orchestra perform, it was hard to believe this was youth talent. The passion, precision, and professionalism on stage were genuinely extraordinary."
+          posterImage="/about-us-quote-poster.jpg"
+          // videoSrc="/about-us-quote-preview.mp4"   //
+          youtubeUrl="https://youtu.be/8HixIOtXEN4?si=N13_yW1Zjo5zVaH-" // changeable
+        />
+      </div>
+
       <EventsBlock />
       <BlogsBlock />
     </main>
