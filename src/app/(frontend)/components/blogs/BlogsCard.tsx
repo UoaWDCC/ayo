7730337b0311af879668edf8f99854e624e53b0f
Kavya Thangella <twkavya004@gmail.com>
2026-08-20 12:13:24 +1200

    fix: added slight padding to blogs card

--- src/app/(frontend)/components/blogs/BlogsCard.tsx
+++ src/app/(frontend)/components/blogs/BlogsCard.tsx
@@ -12,7 +12,7 @@ type Blog = {
 export default function BlogsCard(Blog: Blog) {
   return (
     <Link href={'/news/' + Blog.id}>
-      <div className="border-t border-[#EBEBEB] lg:grid lg:grid-cols-2 py-12 xl:gap-x-70 md:gap-x-20 sm:gap-x-10 hover:bg-gray-50 transition-colors">
+      <div className="border-t border-[#EBEBEB] lg:grid lg:grid-cols-2 py-12 px-4 xl:gap-x-70 md:gap-x-20 sm:gap-x-10 hover:bg-gray-50 transition-colors">
         <div className="2xl:pr-40">
           <h2 className="font-semibold">{Blog.title}</h2>
           <p className="mt-2 text-sm mb-6">{Blog.date}</p>
