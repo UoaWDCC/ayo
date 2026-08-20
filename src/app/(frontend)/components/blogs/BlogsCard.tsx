b5032c63dd542ba4af8c6470e8694cc88487b7f0
Kavya Thangella <twkavya004@gmail.com>
2026-08-20 12:31:58 +1200

    fix: fixed layout of blogs card to match design better

--- src/app/(frontend)/components/blogs/BlogsCard.tsx
+++ src/app/(frontend)/components/blogs/BlogsCard.tsx
@@ -13,7 +13,7 @@ type Blog = {
 export default function BlogsCard(Blog: Blog) {
   return (
     <Link href={'/news' /* TODO: revert to '/news/' + Blog.id once blog detail pages exist */}>
-      <div className="border-t border-[#EBEBEB] lg:grid lg:grid-cols-2 py-12 px-4 xl:gap-x-70 md:gap-x-20 sm:gap-x-10 hover:bg-gray-50 transition-colors">
+      <div className="border-t border-[#EBEBEB] lg:grid lg:grid-cols-2 lg:items-center py-12 px-4 xl:gap-x-70 md:gap-x-20 sm:gap-x-10 hover:bg-gray-50 transition-colors">
         <div className="lg:flex lg:items-center lg:gap-x-8">
           <Image
             className="mb-4 lg:mb-0"
@@ -27,8 +27,8 @@ export default function BlogsCard(Blog: Blog) {
             <p className="mt-2 text-sm mb-6 lg:mb-0">{Blog.date}</p>
           </div>
         </div>
-        <div className="lg:flex lg:justify-end">
-          <p className="italic text-[#]">{Blog.excerpt}</p>
+        <div>
+          <p className="italic text-justify">{Blog.excerpt}</p>
         </div>
       </div>
     </Link>
